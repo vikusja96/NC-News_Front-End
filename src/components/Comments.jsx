74b6d767cee7295f41c64effe6d7402cfcbfdc1e
vikusja96 <vikusja96@gmail.com>
2021-09-19 15:48:12 +0100

    add extra div tags with classNames

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -16,37 +16,43 @@ const Comments = ({ article_id, user }) => {
 
   const handelClick = () => {
     window.scrollTo({
-      top: 100,
+      top: 0,
       behavior: "smooth",
     });
   };
 
   return (
     <section className="Comments">
-      <h2 className="Comments__header">comments</h2>
       <AddComment
         setIsOpen={setIsOpen}
         setComments={setComments}
         article_id={article_id}
         user={user}
       />
+      <div className="Comments__display">
+      <h2 className="Comments__header">Comments</h2>
       <Expandable isOpen={isOpen} setIsOpen={setIsOpen}>
+       <div className="Comments__all">
         <ul className="Comments__list">
           {comments.map(({ comment_id, author, votes, body, created_at }) => {
             return (
-              <li key={comment_id}>
-                <p>{author}</p>
-                <p>{body}</p>
-                <p>{votes}</p>
-                <p>{created_at}</p>
+              <li className="Comments__list-item" key={comment_id}>
+                <p className="Comments__list-author">{author}</p>
+                <p className="Comments__list-body">{body}</p>
+                <div className="Comments__list-details">
+                <p className="Comments__list-likes">👍 &nbsp;{votes}</p>
+                <p className="Comments__list-date">{created_at}</p>
+                </div>
               </li>
             );
           })}
         </ul>
         <button className="Comments__scroll-up" onClick={handelClick}>
-          ^
+        ⬆
         </button>
+        </div>
       </Expandable>
+      </div>
     </section>
   );
 };
