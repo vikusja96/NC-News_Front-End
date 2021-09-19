d93d22b32fcb8abdf962ff5acc5a65cf74c40758
vikusja96 <vikusja96@gmail.com>
2021-09-19 15:38:42 +0100

    add Link tag around Login button

--- src/components/RequireLogin.jsx
+++ src/components/RequireLogin.jsx
@@ -1,15 +1,26 @@
-const RequireLogin = ({user, children, setUser}) => {
-    if (user) return children;
-    return (
-        <div className='Login__home-page'>
-            <h1>Northcoders News</h1>
-            <h2>Please login to use this app</h2>
-            <p>Log-in as grumpy19:</p>
-            <button className='Login__button'onClick={() => setUser({ username: "grumpy19" })}>
-                Log-in
-            </button>
+import { Link } from "react-router-dom";
+
+const RequireLogin = ({ user, children, setUser }) => {
+  if (user) return children;
+  return (
+    <div className="Login__home-page">
+      <h1>
+        Northcoders <span>News</span>
+      </h1>
+      <div className="Login__pop-up">
+        <h2>Please login to use this app</h2>
+        <p>You can Log-in as grumpy19</p>
+        <div className="Login__button-box">
+        <Link to={'/articles'}>
+          <button
+            className="Login__button"
+            onClick={() => setUser({ username: "grumpy19" })}>Log-in
+          </button>
+        </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RequireLogin;
